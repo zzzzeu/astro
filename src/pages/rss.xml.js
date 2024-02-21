478d8f47cd6b37de6623e4ea0b7c4658c2ffdcaa
zzzzeu <zouanei@outlook.com>
2024-02-22 00:17:45 +0800

    feat: update rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,4 +1,5 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
   return rss({
@@ -6,6 +7,12 @@ export async function GET(context) {
     description: 'My journey learning Astro',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
     customData: `<language>en-us</language>`,
-  });
+  })
 }
